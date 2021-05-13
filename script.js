93d1817ed3a46d488c98905ed0993f68be0ac1c9
Areddy28 <reddyanurag28@gmail.com>
2021-05-12 23:57:42 -0400

    Second commit for [Reddy]-part-2

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-/*
-Step 1: prompt user for pets name, 
-        store in variable (name)
-Step 2: prompt user for an action
-        store in variable (action)        
-Step 2: Loop action times 
-        ask user for action 
-        add that action        
-*/
 
 let name = prompt("Name your pet?"); 
 let action = prompt("Feed, Pet, or Walk?")
-let total = 0;
+let total = 0; 
+let happiness = 0;
+let energy = 0;  
+
 for (let i = 0; i< 6; i++) {
     let action = prompt("Enter action"); 
-    total += action; 
-}
-console.log("")
+    console.log(action);
+
+    if (action === "feed") {
+        energy += 2;
+    } else if (action === "pet") {
+    happiness += 1; 
+    } else if (action === "walk") {
+            if (energy < 1) {
+                 console.log("too tired to walk");   
+            } else {
+                happiness +=2;
+                energy -= 1;
+            }
+        } else {
+        }         
+    }
+console.log("happiness"); 
+console.log("energy"); 
