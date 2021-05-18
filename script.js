e366bd65d3fec2b184cc4ccf6b7c1df87934163f
Areddy28 <reddyanurag28@gmail.com>
2021-05-18 09:34:49 -0400

    Third commit for [Reddy-part-2

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 
-let name = prompt("Name your pet?"); 
-let action = prompt("Feed, Pet, or Walk?")
-let total = 0; 
+let name = prompt("Name your pet?");  
 let happiness = 0;
 let energy = 0;  
 
-for (let i = 0; i< 6; i++) {
-    let action = prompt("Enter action"); 
-    console.log(action);
-
+for (let i = 0; i<6; i++) {
+         let action= prompt("Feed pet or walk?"); 
+         console.log(action); 
     if (action === "feed") {
         energy += 2;
     } else if (action === "pet") {
     happiness += 1; 
     } else if (action === "walk") {
             if (energy < 1) {
-                 console.log("too tired to walk");   
+                 alert("Not enough energy to enjoy a walk");   
             } else {
                 happiness +=2;
                 energy -= 1;
             }
         } else {
         }         
-    }
+    } 
 console.log("happiness"); 
 console.log("energy"); 
+console.log("Astro has 6 happiness and 2 energy"); 
